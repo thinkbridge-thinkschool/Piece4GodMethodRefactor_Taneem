807ec385869cfbca18b99b0849fcccae9e44e306
Taneem110 <sajjantaneem19@gmail.com>
2026-05-20 12:38:00 +0530

    Piece 9 Output File

--- src/obj/Debug/net10.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net10.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc1789b10bd513e80b4197b1510f48bf30463e13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df7680235119cebe34afe4886d14d7d00e41428")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/obj/Debug/net10.0/OrderTests.AssemblyInfo.cs
+++ tests/obj/Debug/net10.0/OrderTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc1789b10bd513e80b4197b1510f48bf30463e13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d795c8112597693bf1f184e3afea8c94a38f36d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
